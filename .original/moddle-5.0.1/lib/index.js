4f1ac64f753326173f9d2b230599586b6829313d
hudaokeji <github@hudaokeji.com>
2020-09-15 14:48:42 +0900

    add origial version

---
+++ .original/moddle-5.0.1/lib/index.js
@@ -0,0 +1,13 @@
+export {
+  default as Moddle
+} from './moddle';
+
+export {
+  parseName as parseNameNS
+} from './ns';
+
+export {
+  isBuiltIn as isBuiltInType,
+  isSimple as isSimpleType,
+  coerceType
+} from './types';
